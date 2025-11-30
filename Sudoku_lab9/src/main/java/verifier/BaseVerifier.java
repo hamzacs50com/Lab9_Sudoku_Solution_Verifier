177a62614ad68e9e8161cc4fd298d5b2cad218e8
unknown <medo246ahmed642@gmail.com>
2025-11-30 19:43:35 +0200

    base verifier and sequential verifier done

--- Sudoku_lab9/src/main/java/verifier/BaseVerifier.java
+++ Sudoku_lab9/src/main/java/verifier/BaseVerifier.java
@@ -1,6 +1,25 @@
 package verifier; // Simplified package
 
+import model.ValidationResult;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Locale;
 
 public abstract class BaseVerifier implements Verifier {
-   
+    protected void checkArray(String type, int index, int[] data, ValidationResult result) {
+        Map<Integer, List<Integer>> positionsMap = new HashMap<>();
+        for (int i = 0; i < data.length; i++) {
+            positionsMap.putIfAbsent(data[i], new ArrayList<>());
+            positionsMap.get(data[i]).add(i + 1);
+        }
+        for (Map.Entry<Integer, List<Integer>> entry : positionsMap.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                String errorMsg = String.format(Locale.US, "%s %d, #%d, %s", 
+                    type, index + 1, entry.getKey(), entry.getValue().toString());
+                result.addError(errorMsg);
+            }
+        }
+    }
 }
--- Sudoku_lab9/src/main/java/verifier/SequentialVerifier.java
+++ Sudoku_lab9/src/main/java/verifier/SequentialVerifier.java
@@ -1,6 +1,15 @@
 package verifier; // Simplified package
 
+import model.SudokuBoard;
+import model.ValidationResult;
 
 public class SequentialVerifier extends BaseVerifier {
-    
+    @Override
+    public ValidationResult verify(SudokuBoard board) {
+        ValidationResult result = new ValidationResult();
+        for (int i = 0; i < 9; i++) checkArray("ROW", i, board.getRow(i), result);
+        for (int i = 0; i < 9; i++) checkArray("COL", i, board.getCol(i), result);
+        for (int i = 0; i < 9; i++) checkArray("BOX", i, board.getBox(i), result);
+        return result;
+    }
 }
